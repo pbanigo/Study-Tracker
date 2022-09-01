42f9c11be7906143b0709ef24bb76b51f164e657
Peter <41585899+pbanigo@users.noreply.github.com>
2022-09-01 21:32:36 +0100

    Added Rudimentary User Profile Page
    - Added view
    - Added Route
    - Added Controller

---
+++ controllers/user.js
@@ -0,0 +1,13 @@
+const Todo = require('../models/User')
+
+module.exports = {
+    getUser: async (req,res)=>{
+        console.log(req.user)
+        try{
+            const todoItems = await Todo.find({userId:req.user.id})
+            res.render('profile.ejs', {todos: todoItems, user: req.user})
+        }catch(err){
+            console.log(err)
+        }
+    }
+}    
