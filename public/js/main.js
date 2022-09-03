31853f57acd016087c020cac9df8b69e6195f870
Paul Cabezal <paulcabezal@gmail.com>
2022-09-03 15:46:08 -0400

    add modal and fix footer

--- public/js/main.js
+++ public/js/main.js
@@ -68,3 +68,58 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+const truncator = function (str, len) {
+    if (str.length > len && str.length > 0) {
+        let new_str = str + ' '
+        new_str = str.substr(0, len)
+        new_str = str.substr(0, new_str.lastIndexOf(' '))
+        new_str = new_str.length > 0 ? new_str : str.substr(0, len)
+        return new_str + '...'
+    }
+    return str
+}
+
+// Get modal element
+var modal = document.getElementById('simpleModal');
+var modalBtns = document.getElementsByClassName('modal-test')
+var cardclicks = document.getElementsByClassName('card-clicker')
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// modals.forEach(el => el.addEventListener('click', openModal)) 
+Array.from(modalBtns).forEach((el)=>{
+    el.addEventListener('click', openModal)
+})
+
+Array.from(cardclicks).forEach((el)=>{
+    el.addEventListener('click', openModal)
+})
+
+
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Click outside and close
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
